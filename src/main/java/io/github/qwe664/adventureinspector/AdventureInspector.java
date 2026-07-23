e95a59b5c238ed201c8cd7b215b7408b682d3b66
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-23 14:49:57 +0800

    Register InventoryListener on plugin enable

--- src/main/java/io/github/qwe664/adventureinspector/AdventureInspector.java
+++ src/main/java/io/github/qwe664/adventureinspector/AdventureInspector.java
@@ -1,6 +1,7 @@
 package io.github.qwe664.adventureinspector;
 
 import io.github.qwe664.adventureinspector.command.LoreTestCommand;
+import io.github.qwe664.adventureinspector.listener.InventoryListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdventureInspector extends JavaPlugin {
@@ -9,9 +10,13 @@ public final class AdventureInspector extends JavaPlugin {
     public void onEnable() {
         getLogger().info("AdventureInspector is enabling...");
 
+        // 註冊指令
         if (getCommand("loretest") != null) {
             getCommand("loretest").setExecutor(new LoreTestCommand(this));
         }
+
+        // 註冊事件
+        getServer().getPluginManager().registerEvents(new InventoryListener(), this);
 
         getLogger().info("AdventureInspector enabled successfully.");
     }
