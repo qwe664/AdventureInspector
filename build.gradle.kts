83610e29237f14e018cb95b7cf5039a36124895b
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-23 14:40:35 +0800

    Add Adventure MiniMessage dependency
    Added Adventure MiniMessage dependency to the project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,14 @@ version = "0.1.0-MVP"
 
 repositories {
     mavenCentral()
-
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+
+    // Adventure MiniMessage
+    compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
 }
 
 java {
