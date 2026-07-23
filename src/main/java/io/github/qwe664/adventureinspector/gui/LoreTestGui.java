2582f3dd6a515c12c7acb210c7c29c60cf57e488
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-23 14:36:51 +0800

    Add plainTextItem to LoreTestGui inventory

--- src/main/java/io/github/qwe664/adventureinspector/gui/LoreTestGui.java
+++ src/main/java/io/github/qwe664/adventureinspector/gui/LoreTestGui.java
@@ -1,5 +1,6 @@
 package io.github.qwe664.adventureinspector.gui;
 
+import io.github.qwe664.adventureinspector.item.LoreTestItems;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -11,6 +12,9 @@ public class LoreTestGui {
 
     public static void open(Player player) {
         Inventory inventory = Bukkit.createInventory(null, 27, "AdventureInspector");
+
+        // 第一個測試物品
+        inventory.setItem(10, LoreTestItems.plainTextItem());
 
         player.openInventory(inventory);
     }
