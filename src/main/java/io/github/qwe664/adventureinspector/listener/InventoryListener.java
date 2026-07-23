5f6aea7811cda55ba471c1dfa23742f0a9a5d026
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-23 14:47:38 +0800

    Add InventoryListener to cancel clicks on specific inventory

---
+++ src/main/java/io/github/qwe664/adventureinspector/listener/InventoryListener.java
@@ -0,0 +1,16 @@
+package io.github.qwe664.adventureinspector.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+
+        if (event.getView().getTitle().equals("AdventureInspector")) {
+            event.setCancelled(true);
+        }
+    }
+}
