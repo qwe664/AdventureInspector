a1691b564f6103e76890f223307394e3a542fb7f
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-23 14:56:02 +0800

    Refactor LoreTestGui to include plugin parameter
    Updated the open method to accept an AdventureInspector plugin parameter and modified item retrieval to use the plugin.

--- src/main/java/io/github/qwe664/adventureinspector/gui/LoreTestGui.java
+++ src/main/java/io/github/qwe664/adventureinspector/gui/LoreTestGui.java
@@ -1,21 +1,22 @@
 package io.github.qwe664.adventureinspector.gui;
 
+import io.github.qwe664.adventureinspector.AdventureInspector;
 import io.github.qwe664.adventureinspector.item.LoreTestItems;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
-public class LoreTestGui {
+public final class LoreTestGui {
 
     private LoreTestGui() {
     }
 
-    public static void open(Player player) {
+    public static void open(Player player, AdventureInspector plugin) {
         Inventory inventory = Bukkit.createInventory(null, 27, "AdventureInspector");
 
-        inventory.setItem(10, LoreTestItems.plainTextItem());
-        inventory.setItem(12, LoreTestItems.miniMessageItem());
-        inventory.setItem(14, LoreTestItems.literalItem());
+        inventory.setItem(10, LoreTestItems.plainTextItem(plugin));
+        inventory.setItem(12, LoreTestItems.miniMessageItem(plugin));
+        inventory.setItem(14, LoreTestItems.literalItem(plugin));
 
         player.openInventory(inventory);
     }
